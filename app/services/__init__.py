c809c182d4ffc4a3871f3bcecb2612c3c150b44b
daTaHere <adamhuy123@yahoo.com>
2025-11-30 18:39:42 -0800

    test(feed_service): Remove internal helper from public API exposure

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -3,12 +3,11 @@ Services package initialization.
 """
 
 from app.services.user_service import create_user, get_all_users, get_user_by_id
-from app.services.feed_service import create_model_list, get_10_feeds
+from app.services.feed_service import get_10_feeds
 
 __all__ = [
     "get_all_users",
     "create_user",
     "get_user_by_id",
     "get_10_feeds",
-    "create_model_list",
 ]
