bc9d8b8097f6df1b9751e7eca7ab1ee818e56106
Adam Huynh <127999910+daTaHere@users.noreply.github.com>
2026-01-23 11:43:48 -0800

    refactor(feeds): harden service and updated unit test for create_model_list (#34)
    * refactor(feeds): harden service and updated unit test for create_model_list
    
    * refactor(fees): add schema field on ServiceValidationExecption for tracking

--- app/services/feeds/feed_builders.py
+++ app/services/feeds/feed_builders.py
@@ -3,8 +3,9 @@
 from typing import Any, Dict, List, Type, TypeVar
 
 from app.models import Post, Comment
-from app.exceptions.base import ServiceException
-from app.exceptions.exception_handlers import handle_service_error
+
+from app.exceptions.service import ServiceInternalException, ServiceValidationException
+from app.exceptions.exception_handlers import handle_service_errorV2
 from app.utils.logger_helper import handle_log
 
 
@@ -15,6 +16,9 @@ def create_model_list(input_data: List[Dict[str, Any]], model: Type[T]) -> List[
     """
     Instantiate Post or Comment objects from raw data and return a list of model instances.
     """
+
+    items: List[T] = []
+
     try:
         handle_log(
             "Creating list of model instances",
@@ -23,17 +27,23 @@ def create_model_list(input_data: List[Dict[str, Any]], model: Type[T]) -> List[
             service_method="create_model_list",
             model=model.__name__,
         )
-        items = [model(d) for d in input_data]
+        items = [model(data) for data in input_data]
 
-    except (TypeError, ValueError) as e:
-        handle_service_error(
+    except (KeyError, TypeError, ValueError) as e:
+        handle_service_errorV2(
             e,
-            f"Internal Server Error: Failed to create {model.__name__} instances.",
             f"Error creating {model.__name__} instances",
-            exc_type=ServiceException,
+            exc_type=ServiceValidationException,
+            service_method="create_model_list",
+            schema=model.__name__,
+        )
+    except AttributeError as e:
+        handle_service_errorV2(
+            e,
+            f"Attribute error creating {model.__name__} instances",
+            exc_type=ServiceInternalException,
             service_method="create_model_list",
             model=model.__name__,
-            method="create_model_list",
         )
 
     handle_log(
